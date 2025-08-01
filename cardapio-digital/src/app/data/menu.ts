40c5613a72228d54c0b1e20224ea105dddf68a1a
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-08-01 21:01:28 +0000

    thrid commit

--- cardapio-digital/src/app/data/menu.ts
+++ cardapio-digital/src/app/data/menu.ts
@@ -1,7 +1,7 @@
 export type Product = {
   id: number;
   nome: string;
-  preco: string;
+  preco: number;
   descricao: string;
 };
 
@@ -9,39 +9,39 @@ export const menu: Product[] = [
   {
     id: 1,
     nome: "Hambúrguer Artesanal",
-    preco: "Um beijo",
+    preco: 25,
     descricao: "Pão , carne 180g, cheddar e bacon e onion rings",
   },
   {
     id: 2,
     nome: "Pizza Caseira",
-    preco: "dois beijos",
+    preco: 25,
     descricao: "Molho de tomate, muçarela, requeijão e gorgonzola",
   },
   {
     id: 3,
     nome: "Pastel do marido",
-    preco: "três beijos",
+    preco: 25,
     descricao: "Carne, queijo e pizza",
   },
   {
     id: 4,
     nome: "Coxa com sobrecoxa recheada",
-    preco: "quatro beijos",
+    preco: 25,
     descricao: "Coxa com sobrecoxa empanada recheada com presunto e queijo",
   },
 
   {
     id: 5,
     nome: "Manta de linguiça com queijo",
-    preco: "quatro beijos",
+    preco: 25,
     descricao: "Linguiça e queijo (bem óbvio né)",
   },
 
   {
     id: 6,
     nome: "Manta de linguiça com queijo",
-    preco: "quatro beijos",
+    preco: 25,
     descricao: "Linguiça e queijo (bem óbvio né)",
   },
 ];
