050299226eb3ebdbc0836a3e5b20c8a00e031faf
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-08-01 21:30:39 +0000

    adição do metadata no layout.tsx

--- cardapio-digital/src/app/layout.tsx
+++ cardapio-digital/src/app/layout.tsx
@@ -2,6 +2,11 @@ import "./globals.css"
 import { ReactNode } from "react"
 import { CartProvider } from "./context/CartContext"
 
+export const metadata = {
+  title: "cardapio digital",
+  description: "Veja nosso cardapio e faça seu pedido",
+}
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
