4d3874c9c8f4b7e70ef8137cd5a728c385b1774c
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-08-01 21:19:07 +0000

    ;

--- cardapio-digital/src/app/page.tsx
+++ cardapio-digital/src/app/page.tsx
@@ -8,7 +8,7 @@ import WelcomeScreen from "./components/WelcomeScreen";
 export default function Home() {
   return (
     <>
-      <WelcomeScreen />;
+      <WelcomeScreen />
       <div className="min-h-screen bg-gray-100 p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {menu.map((item) => (
           <ProductCard key={item.id} product={item} />
